9ea223df563ced2ab383a7dc1f69f5b0d76fc94c
unknown <jshigoodies@gmail.com>
2021-06-05 15:54:46 -0700

    handling events

--- First_Bot/src/jshi/Jason/Minecraft_BOT/Commands.java
+++ First_Bot/src/jshi/Jason/Minecraft_BOT/Commands.java
@@ -1,21 +1,38 @@
 package jshi.Jason.Minecraft_BOT;
 
+import net.dv8tion.jda.api.entities.Invite.Guild;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class Commands extends ListenerAdapter{
-	
 	@Override
 	public void onMessageReceived(MessageReceivedEvent event)
-	{
+	{	
 		Message msg = event.getMessage();
+		
+		MessageChannel channel = event.getChannel(); //these are all pretty self explanatory
+		
 		if(msg.getContentRaw().equals("!ping"))
 		{
-			MessageChannel channel = event.getChannel(); //these are all pretty self explanatory
+			String username = event.getAuthor().getName();
 			long time = System.currentTimeMillis();
-			channel.sendMessage("Pong! " + time);
+			channel.sendMessage("Pong! \n" + username + "\n" + time).queue();
+			channel.sendMessage(":D").queue();
 		}
+		
+		if(msg.getContentRaw().equals("!kill me"))
+		{
+			channel.sendMessage("k").queue();
+		}
+			
 	}
+	
+    @Override
+    public void onGuildJoin(GuildJoinEvent event) {
+        Guild g = (Guild) event.getGuild();
+        System.out.println("Joined new server "+g.getName());
+    }
 }
--- First_Bot/src/jshi/Jason/Minecraft_BOT/Main.java
+++ First_Bot/src/jshi/Jason/Minecraft_BOT/Main.java
@@ -10,18 +10,11 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.events.GenericEvent;
-import net.dv8tion.jda.api.events.ReadyEvent;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.hooks.EventListener;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class Main extends ListenerAdapter /*implements EventListener*/{
+public class Main /*implements EventListener*/{
 	//ctrl + shift + o --> automatically imports everything
 	public static JDA jda;
-	private String prefix = ".";
+	private static String prefix = ".";
 	
 	public static void main(String[] args) throws LoginException, InterruptedException
 	, IOException {
@@ -31,31 +24,14 @@ public class Main extends ListenerAdapter /*implements EventListener*/{
 		
 		String token = input.nextLine();
 		
-		jda = JDABuilder.createDefault(token).addEventListeners(new Main()).build(); //wow that is hard to read
+		jda = JDABuilder.createDefault(token).addEventListeners(new Commands()).build(); //wow that is hard to read
 		jda.getPresence().setStatus(OnlineStatus.DO_NOT_DISTURB);
 		jda.getPresence().setActivity(Activity.playing("ANIME ANIME ANIME"));
 		//jda.addEventListener(new Commands());
-		jda.addEventListener(new Main());
 	}
-
-//	@Override
-//	public void onEvent(GenericEvent event) {
-//		// TODO Auto-generated method stub
-//		if (event instanceof ReadyEvent)
-//            System.out.println("API is ready!");
-//	}
-
-	@Override
-	public void onMessageReceived(MessageReceivedEvent event)
+	
+	public String getPrefix()
 	{
-		Message msg = event.getMessage();
-		if(msg.getContentRaw().equals("!ping"))
-		{
-			MessageChannel channel = event.getChannel(); //these are all pretty self explanatory
-			long time = System.currentTimeMillis();
-			channel.sendMessage("Pong! " + time).queue();		
-		}
+		return prefix;
 	}
-	
-	
 }
