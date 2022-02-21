a5a62903686346cf9e8b22da0e61fa513e5f206b
unknown <jshigoodies@gmail.com>
2022-02-21 02:20:55 -0600

    making a new bot

---
+++ Queue_Bot/src/jshi/Jason/Queue_Bot/Main.java
@@ -0,0 +1,16 @@
+package jshi.Jason.Queue_Bot;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class Main extends ListenerAdapter{
+	
+	public static JDA jda;
+	public static String prefix = "!";
+	
+	
+	public static void main(String[] args)
+	{
+		
+	}
+}
